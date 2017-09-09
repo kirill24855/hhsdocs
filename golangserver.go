546d7f91732d056969d07ab9bc58f18e8f7c3137
calba5141114 <soradev4@gmail.com>
2017-09-08 20:36:11 -0700

    Update golangserver.go
    updated server

--- golangserver.go
+++ golangserver.go
@@ -1,15 +1,26 @@
-
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
-func main (){
-	http.HandleFunc("/",someFunc)
-	http.ListenAndServe(":8080",nil)
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	data, err := ioutil.ReadFile("index.html")
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Length", fmt.Sprint(len(data)))
+	fmt.Fprint(w, string(data))
 }
 
-func someFunc(w http.ResponseWriter,req *http.Request)  {
-	w.Write([]byte("Hello Universe"))
+func main() {
+	http.HandleFunc("/", rootHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 
